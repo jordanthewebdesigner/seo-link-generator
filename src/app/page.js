f4f451f13f0672d5797b28901268bc820c120e09
jordanthewebdesigner <jordanthewebdesigner@gmail.com>
2024-05-27 14:49:30 +0000

    Fixes click to copy bug

--- src/app/page.js
+++ src/app/page.js
@@ -276,10 +276,14 @@ export default function Home() {
             <div
               className="inline-flex items-center justify-center w-full "
               key={index}
-              onClick={handleClickCopy(`${linkInfo.keyword} | ${linkInfo.location} | ${phoneNumber}`)}
             >
               <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900 hover:underline ">
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900 hover:underline "
+              
+              onClick={ () => {
+                handleClickCopy(`${linkInfo.keyword} | ${linkInfo.location} | ${phoneNumber}`)
+              }}
+              >
                 {linkInfo.keyword} | {linkInfo.location} | {phoneNumber} 
               </span>
             </div>
